727ac87b598270a614e564a1c23352a66caf5839
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-03-29 10:26:21 +0200

    changed Hello World to HelloYou
    change "World"  via args

---
+++ HelloWorlds/HelloYou.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+class HelloWorldApp {
+	public static void main(String[] args) {
+		ArrayList<String> input=new ArrayList<String>();
+		if (args.length<1) {input.add("You");}
+		else {input.add(args[0]);}
+		System.out.println("Hello "+input.get(0) +"!");
+	}
+}
