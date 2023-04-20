0e6f310eaf4ff9b6cb930d915038b876544cc2d2
Zapdos333 <adrian.martin1@gmx.de>
2023-04-20 20:00:01 +0200

    Update run.bat
    removing unused labels
    add blank lines for easier reading

--- run.bat
+++ run.bat
@@ -5,16 +5,18 @@ set /P jpath=please enter the path from \com\Ace009\ in "" :
 echo compiling...
 javac com\Ace009\%jpath%\*.java
 echo files in path:
+echo:
 dir "com\Ace009\%jpath%\"
+echo:
 set /P jname=name of entry class : 
 set /P jargs=arguments for main : 
 echo executing...
+echo:
 java "com.Ace009.%jpath:\=.%.%jname%" %jargs%
-:end
+echo:
 choice /c CRS /m "C for closing the window; R running again from same path; S for running a different file"
 if errorlevel 3 (goto start)
 if errorlevel 2 (goto rerun)
-:close
 echo closing...
 pause
 exit
