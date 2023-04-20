fc409ba8a15b7d6406c2403139a2d2b9b46287c4
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-04-20 13:09:04 +0200

    Update run.bat for package pathing

--- run.bat
+++ run.bat
@@ -1,18 +1,16 @@
 @echo off
 :start
-set /P jpath=please enter the (relative)filepath in "" : 
+set /P jpath=please enter the path from \com\Ace009\ in "" : 
+:rerun
+set /P jname=name of entry class point: 
 set /P jargs=arguments for main : 
-java %jpath% %jargs%
+javac "com\Ace009\%jpath%\*.java"
+java "com.Ace009.%jpath:\=.%.%jname%" %jargs%
 :end
-choice /c CRS /m "C for closing the window; R running again after pause; S for running a different file"
+choice /c CRS /m "C for closing the window; R running again from same path; S for running a different file"
 if errorlevel 3 (goto start)
 if errorlevel 2 (goto rerun)
 :close
 echo closing...
 pause
 exit
-:rerun
-echo waitung for restart...
-pause
-java %jpath% %jargs%
-goto end
