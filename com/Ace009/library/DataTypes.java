9283b87fc08c8b8ce2e611db3c90dd7d2060438d
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-04-24 10:42:41 +0200

    Update DataTypes.java
    add Circle(WIP)

--- com/Ace009/library/DataTypes.java
+++ com/Ace009/library/DataTypes.java
@@ -1,6 +1,6 @@
 package com.Ace009.library;
 
-public class Coordinate  {
+class Coordinate  {
 	float x;
 	float y;
 	public Coordinate(float f_x, float f_y) {
@@ -9,5 +9,34 @@ public class Coordinate  {
 	}
 	public Coordinate cordsInt(int f_x, int f_y) {
 		return new Coordinate((float)f_x,(float)f_y);
+	}
+}
+class Circle {
+	Coordinate center;
+	float radius;
+	public Circle(Coordinate f_Center, float r) {
+		center=f_Center;
+		radius=r;
+	}
+	public Coordinate positionDegrees(float degrees) {
+		float x;
+		float y;
+		if (degrees%360 > 0) {
+			degrees= degrees-((degrees%360)*degrees);
+		}
+		if (degrees < 90) {
+			
+		} else if (degrees < 180) {
+			
+		} else if (degrees < 270) {
+			
+		} else if (degrees < 360) {
+			
+		} else {
+			throw new Exception("invalid degree number");
+		}
+	}
+	public static void main(String[] args) {
+
 	}
 }
