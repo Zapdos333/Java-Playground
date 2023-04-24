820cf090731ec0b65eb4f79214022958a83f47c3
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-04-24 10:33:07 +0200

    Create Coordinate.java
    library for DataType Coordinate

---
+++ com/Ace009/library/Coordinate.java
@@ -0,0 +1,10 @@
+package com.Ace009.library;
+class Coordinate  {
+	float x;float y;
+	public Coordinate(float f_x, float f_y) {
+		x=f_x;y=f_y;
+	}
+	public Coordinate cordsInt(int f_x, int f_y) {
+		return new Coordinate((float)f_x,(float)f_y);
+	}
+}
