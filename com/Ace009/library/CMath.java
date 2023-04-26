c212490f52514072b0fe3760b5d1c211eb9c6976
Zapdos333 <adrian.martin1@gmx.de>
2023-04-26 19:27:04 +0200

    Hotfix
    #3
    make CMath public to be accesible by other packages

--- com/Ace009/library/CMath.java
+++ com/Ace009/library/CMath.java
@@ -1,6 +1,6 @@
 package com.Ace009.library;
 
-class CMath {
+public class CMath {
 	public static int limitedRandom(int min, int max) {
 		Double random=Math.random()*((max+1)-min)+min;
 		return random.intValue();
