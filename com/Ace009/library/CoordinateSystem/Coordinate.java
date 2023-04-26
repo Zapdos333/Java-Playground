c52d1808a961fd16f30b9c28070f0689d612db06
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-04-26 11:58:09 +0200

    work_today_2
    changed all float to double for accuracy
    added [at]Override to Coordinate Object
    tested Overrides

--- com/Ace009/library/CoordinateSystem/Coordinate.java
+++ com/Ace009/library/CoordinateSystem/Coordinate.java
@@ -1,7 +1,20 @@
 package com.Ace009.library.CoordinateSystem;
 class Coordinate {
-	float x;float y;
-	public Coordinate(float f_x, float f_y) {
+	double x;double y;
+	public Coordinate(double f_x, double f_y) {
 		x=f_x;y=f_y;
+	}
+	@Override
+	public String toString() {
+		return "{x:"+this.x+", y:"+this.y+"}";
+	}
+	public boolean equals(Object o) {
+		if (o==this) {return true;}
+		if (!(o instanceof Coordinate)) {return false;}
+		Coordinate c = (Coordinate) o;
+		return Double.compare(this.x,c.x)==0 && Double.compare(this.y,c.y)==0;
+	}
+	public int hashCode() {
+		return Double.hashCode(x)+Double.hashCode(y);
 	}
 }
