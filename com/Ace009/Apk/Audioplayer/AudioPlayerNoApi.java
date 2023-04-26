3577003426a0da0f5b0519318f6cda5e26ce8828
Zapdos333 <adrian.martin1@gmx.de>
2023-04-26 19:09:49 +0200

    Update AudioPlayerNoApi.java
    #3
    className->TrackManager
    Song->Track

--- com/Ace009/Apk/Audioplayer/AudioPlayerNoApi.java
+++ com/Ace009/Apk/Audioplayer/AudioPlayerNoApi.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import com.Ace009.library.CMath;
 
-class AudioPlayer {
-	//define Song Object
-	private class Song {
+class TrackManager {
+	ArrayList<Track> history=new ArrayList<Track>;
+	ArrayList<Track> playlist=new ArrayList<Track>;
+	//define Music Track Object
+	public class Track {
 		int id;	//id as number in (play)list
 		String fName;	//file name
 		String fPath; //file path
 		int[] length; //array [sec,min]
-		ArrayList<Integer> fitsToId; //ArrayList of ids of Songs that fit
-		ArrayList<Integer> noFitId; //ArrayList of ids of Songs that don't fit
-		public Song(int index, String f_name, String f_path, int lengthMin, int lengthSec) {
+		ArrayList<String> fitsToName; //ArrayList of fNames of Tracks that fit
+		ArrayList<String> noFitName; //ArrayList of fNames of Tracks that don't fit
+		float amplification;
+		public Track(int index, String f_name, String f_path, int lengthMin, int lengthSec) {
+			amplification=1;
 			id=index;
 			fName=f_name;
 			fPath=f_path;
@@ -22,7 +26,18 @@ class AudioPlayer {
 			length[1]=lengthMin;
 		}
 	}
-	//debug Main function
+	//reset playlist method
+	public void reset(boolean full) {
+		if (full) {
+			playlist.forEach(element->{
+				element.fitsToName=ArrayList<String>;
+				element.noFitName=ArrayList<String>;
+				element.amplification=1;
+			});
+		}
+		history=new ArrayList<Track>();
+	}
+	//debug Main method
 	public static void main(String[] args) {
 		for (int i = 1; i <= 15; i++) {
 			System.out.println("Iteration: "+i);
