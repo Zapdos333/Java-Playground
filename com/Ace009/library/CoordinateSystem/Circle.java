4029832211f0abefcd76aa361a15607217e1ca28
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-04-26 11:58:09 +0200

    work_today_2
    changed all float to double for accuracy
    added [at]Override to Coordinate Object
    tested Overrides

--- com/Ace009/library/CoordinateSystem/Circle.java
+++ com/Ace009/library/CoordinateSystem/Circle.java
@@ -4,33 +4,33 @@ import java.util.ArrayList;
 
 class Circle {
 	Coordinate center;
-	float radius;
-	public Circle(Coordinate f_Center, float r) {
+	double radius;
+	public Circle(Coordinate f_Center, double r) {
 		center=f_Center;
 		radius=r;
 	}
-	public Coordinate positionDegrees(float degrees) {
-		float deltax= 0;
-		float deltay= 0;
+	public Coordinate positionDegrees(double degrees) {
+		double deltax= 0;
+		double deltay= 0;
 		degrees= degrees%360;
 		if (degrees < 90) {
-			deltax=radius*(float)Math.sin(Math.toRadians(degrees));
-			deltay=radius*(float)Math.cos(Math.toRadians(degrees));
+			deltax=radius*Math.sin(Math.toRadians(degrees));
+			deltay=radius*Math.cos(Math.toRadians(degrees));
 		} else if (degrees < 180) {
-			deltay=radius*(float)Math.sin(Math.toRadians(degrees));
-			deltax=radius*(float)Math.cos(Math.toRadians(degrees));
+			deltay=radius*Math.sin(Math.toRadians(degrees));
+			deltax=radius*Math.cos(Math.toRadians(degrees));
 		} else if (degrees < 270) {
-			deltax=radius*(float)Math.sin(Math.toRadians(degrees));
-			deltay=radius*(float)Math.cos(Math.toRadians(degrees));
+			deltax=radius*Math.sin(Math.toRadians(degrees));
+			deltay=radius*Math.cos(Math.toRadians(degrees));
 		} else if (degrees < 360) {
-			deltay=radius*(float)Math.sin(Math.toRadians(degrees));
-			deltax=radius*(float)Math.cos(Math.toRadians(degrees));
+			deltay=radius*Math.sin(Math.toRadians(degrees));
+			deltax=radius*Math.cos(Math.toRadians(degrees));
 		}
 		return new Coordinate(center.x+deltax,center.y+deltay);
 	}
-	public ArrayList<Coordinate> construct(float interval) {
+	public ArrayList<Coordinate> construct(double interval) {
 		ArrayList<Coordinate> output = new ArrayList<Coordinate>();
-		for(float i=0; i<360;i=i+interval) {
+		for(double i=0; i<360;i=i+interval) {
 			output.add(this.positionDegrees(i));
 		}
 		return output;
@@ -38,16 +38,20 @@ class Circle {
 	public static void main(String[] args) {
 		System.out.println("Test program: ");
 		System.out.println("1: CenterX; 2: CenterY; 3: Radius; 4: DegreesInterval");
-		float aX= Float.parseFloat(args[0]);
-		float aY= Float.parseFloat(args[1]);
-		float aR= Float.parseFloat(args[2]);
-		float aD= Float.parseFloat(args[3]);
+		double aX= Double.parseDouble(args[0]);
+		double aY= Double.parseDouble(args[1]);
+		double aR= Double.parseDouble(args[2]);
+		double aD= Double.parseDouble(args[3]);
 		Circle test = new Circle(new Coordinate(aX,aY),aR);
 		System.out.print("CenterX="+test.center.x);
 		System.out.print(" CenterY="+test.center.y);
 		System.out.print(" Radius="+test.radius);
 		System.out.println(" Degrees="+(aD%360));
 		ArrayList<Coordinate> result = test.construct(aD);
-		System.out.println("");
+		System.out.print("HashCodes:[");
+		result.forEach(element->{
+			System.out.print(element.hashCode()+", ");
+		});
+		System.out.println("]");
 	}
 }
