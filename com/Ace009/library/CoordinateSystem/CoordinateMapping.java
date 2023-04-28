8061bd9b645bbf49e737ddabcb4ddbbcc48eceaa
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-04-28 09:06:41 +0200

    commit, yesterday_work
    Position::Coordinate
    map: create map
    inputList: input Object at List->Coordinate
    custom ToString

--- com/Ace009/library/CoordinateSystem/CoordinateMapping.java
+++ com/Ace009/library/CoordinateSystem/CoordinateMapping.java
@@ -3,20 +3,49 @@ package com.Ace009.library.CoordinateSystem;
 import java.util.ArrayList;
 
 final class CoordinateMap {
+	ArrayList<ArrayList<Position>> map=new ArrayList<ArrayList<Position>>();
 	public class Position {
-		double x;double y;Object content;
+		Coordinate pos ;Object content;
 		public Position(Coordinate coords, Object cont) {
-			content=cont;x=coords.x;y=coords.y;
+			content=cont;pos=coords;
+		}
+		@Override
+		public String toString() {
+			return pos.toString()+":"+content.toString();
 		}
 	}
-	public static ArrayList<ArrayList<Position>> createMap (float scale, int sizeX, int sizeY) {
-		ArrayList<ArrayList<Position>> output= new ArrayList<ArrayList<Position>>();
-		for (int i1=0; i1<output.size(); i1++) {
-			for (int i2=0; i2<output.get(0).size(); i2++) {
-				output.get(i1).get(i2).x=scale*i2;
-				output.get(i1).get(i2).y=scale*i1;
+	public CoordinateMap (int sizeX, int sizeY) {
+		for (int i1=0; i1<sizeY; i1++) {
+			this.map.add(new ArrayList<Position>());
+			for (int i2=0; i2<sizeX; i2++) {
+				this.map.get(i1).add(new Position(new Coordinate(i2,i1)," "));
 			}
 		}
-		return output;
+	}
+	public void inputList(ArrayList<Coordinate> list,Object f_content) {
+		for (Coordinate pos: list) {
+			this.map.get((int)pos.y).get((int)pos.x).content=f_content;
+			Coordinate coord=map.get((int)pos.y).get((int)pos.x).pos;
+			if (!(pos.x==coord.x)) {
+				System.out.println("unequal x");
+			} if (!(pos.y==coord.y)) {
+				System.out.println("unequal y");
+			}
+		}
+	}
+	@Override
+	public String toString() {
+		StringBuilder outputB= new StringBuilder();
+		outputB.append("[");
+		for (ArrayList<Position> row: map) {
+			outputB.append("[");
+			for (Position location: row) {
+				outputB.append(location.toString()+",");
+			}
+			outputB.append("],\n");
+		}
+		outputB.deleteCharAt(outputB.length()-1);
+		outputB.append("]");
+		return outputB.toString();
 	}
 }
