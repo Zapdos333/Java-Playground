476af5fbdbc1402829ea4345f4d43ba51d5e9d16
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-04-28 09:05:02 +0200

    commit, yesterday_work
    Circle: fixed counstruction Array-intLimit and interval; fixed constructPoly interval division-accuracy; added getCircularity; updated main(debug)

--- com/Ace009/library/CoordinateSystem/Circle.java
+++ com/Ace009/library/CoordinateSystem/Circle.java
@@ -30,27 +30,53 @@ class Circle {
 	}
 	public ArrayList<Coordinate> construct(double interval) {
 		ArrayList<Coordinate> output = new ArrayList<Coordinate>();
-		for(double i=0; i<360;i=i+interval) {
-			output.add(this.positionDegrees(i));
+		System.out.println("Debug, interval: "+interval);
+		if (interval<0) {interval=Math.abs(interval);}
+		if (interval=0) {interval=360;}
+		if (interval>360) {interval=interval%360;}
+		while (true) {
+			try {
+				output.ensureCapacity((int)Math.ceil(360/interval));
+			} catch (java.lang.OutOfMemoryError e) {
+				interval=360/((360/interval)-1);
+				System.out.println("retry, interval: "+interval);
+				continue;
+			} finally {
+				for(int i=0; i*interval<360;i++) {
+					output.add(this.positionDegrees(i*interval));
+				}
+				return output;			
+			}
 		}
-		return output;
 	}
 	public ArrayList<Coordinate> constructPoly(int corners) {
-		return this.construct(360/corners);
+		final double circ=360.0;
+		return this.construct(circ/(double)corners);
+	}
+	public static String getCircularity(ArrayList<Coordinate> polygon, double radius) {
+		double circumferance= Coordinate.totalDistance(polygon,true);
+		final double two=2;
+		double circleU=Math.PI*radius*two;
+		return circumferance+"/"+circleU;
 	}
 	public static void main(String[] args) {
 		System.out.println("Test program: ");
-		System.out.println("1: CenterX; 2: CenterY; 3: Radius; 4: Corners");
-		double aX= Double.parseDouble(args[0]);
-		double aY= Double.parseDouble(args[1]);
-		double aR= Double.parseDouble(args[2]);
-		int aC= Integer.parseInt(args[3]);
+		System.out.println("1: Corners");
+		int aC= Integer.parseInt(args[0]);
+		double aX= 5;
+		double aY= 5;
+		double aR= 5;
 		Circle test = new Circle(new Coordinate(aX,aY),aR);
 		System.out.print("CenterX="+test.center.x);
 		System.out.print(" CenterY="+test.center.y);
-		System.out.print(" Radius="+test.radius);
-		System.out.println(" Corners="+(aC));
+		System.out.println(" Radius="+test.radius);
 		ArrayList<Coordinate> result = test.constructPoly(aC);
+		//result= Coordinate.roundCoordList(result,(float)1);
 		System.out.println("Circle: "+result.toString());
+		System.out.println("Circluarity: "+getCircularity(result,test.radius));
+		System.out.println("Corners: "+result.size());
+		/*CoordinateMap map= new CoordinateMap((int)aR*2,(int)aR*2);
+		map.inputList(result,"Circle");
+		System.out.println(map.toString()); */
 	}
 }
