02c03462c7257df091d684d431c64c486f1fd5ac
Zapdos333 <adrian.martin1@gmx.de>
2023-05-01 14:14:51 +0200

    Update TrackManager.java
    added 'database-like' allTracks List
    changed other TrackList to contain Integer(id in allTracks)
    changed getNextTrack accordingly
    created setFitValue, to set the fit and noFit List

--- com/Ace009/Apk/Audioplayer/TrackManager.java
+++ com/Ace009/Apk/Audioplayer/TrackManager.java
@@ -6,22 +6,20 @@ import com.Ace009.library.CClass.CMath;
 import com.Ace009.library.CClass.CList;
 
 public final class TrackManager {
-	ArrayList<Track> history=new ArrayList<Track>();
-	ArrayList<Track> playlist=new ArrayList<Track>();
+	ArrayList<Integer> history=new ArrayList<Integer>();	//list of ids in allTracks
+	ArrayList<Integer> playlist=new ArrayList<Integer>();	//""
+	ArrayList<Track> allTracks=new ArrayList<Track>();
 	//define Music Track Object
 	public class Track {
-		int id;	//id as number in (play)list
 		String fName;	//file name
 		String fPath; //file path
 		int[] length; //array [sec,min]
-		ArrayList<String> fitsToName; //ArrayList of fNames of Tracks that fit
-		ArrayList<String> noFitName; //ArrayList of fNames of Tracks that don't fit
+		ArrayList<Integer> fitsToName=new ArrayList<Integer>(); //ArrayList of allTrackIds of Tracks that fit
+		ArrayList<Integer> noFitName=new ArrayList<Integer>(); //ArrayList of allTrackIds of Tracks that don't fit
 		float amplification;
-		public Track(int index, String f_name, String f_path, int lengthMin, int lengthSec) {
+		public Track(String f_name, String f_path, int lengthMin, int lengthSec) {
+			//id is index in _allTracks_
 			amplification=1;
-			fitsToName=new ArrayList<String>();
-			noFitName=new ArrayList<String>();
-			id=index;
 			fName=f_name;
 			fPath=f_path;
 			length=new int[2];
@@ -32,24 +30,44 @@ public final class TrackManager {
 	//reset playlist method
 	public void reset(boolean full) {
 		if (full) {
-			playlist.forEach(element->{
+			allTracks.forEach(element->{
 				element.fitsToName=new ArrayList<String>();
 				element.noFitName=new ArrayList<String>();
 				element.amplification=1;
 			});
-			playlist=new ArrayList<Track>();
+			playlist=new ArrayList<Integer>();
 		}
-		history=new ArrayList<Track>();
+		history=new ArrayList<Integer>();
 	}
-	public Track getNextTrack(Track currTrack) {
-		Track output;
+	//method to get a random next Track based on fits
+	public int getNextTrack(Track currTrack) {
+		int output;
 		if (CMath.limitedRandom(0,3)>1) {
 			String outputName=CList.getRandom(currTrack.fitsToName);
-			output=playlist.stream().filter(element->element.fName.equals(outputName)).findFirst().get();
+			output=-1;
+			for (int i=0; i<allTracks.size(); i++) {
+				if (allTracks.get(i).fName.equals(outputName)) {
+					output=i;
+				}
+			}
+			if (output==-1) {
+				output=CList.getRandom(playlist);
+			}
 		} else {
 			output=CList.getRandom(playlist);
 		}
 		return output;
+	}
+	//method to set the values int 
+	public void setFitValue(int fitValue, int currTrack, int nextTrack) {
+		switch(fitValue) {
+			case 1:
+				allTracks.get(currTrack).fitsToName.add(playlist.get(nextTrack));
+				break;
+			case -1:
+				allTracks.get(currTrack).noFitName.add(playlist.get(nextTrack));
+				break;
+		}
 	}
 	//debug Main method
 	public static void main(String[] args) {
