983c49d3ea98912d8175bb5e4d282e121a306c86
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-05-04 11:44:14 +0200

    Update AbstractNumberSequence.java
    hotfix

--- com/Ace009/school/math/AbstractNumberSequence.java
+++ com/Ace009/school/math/AbstractNumberSequence.java
@@ -2,18 +2,18 @@ package com.Ace009.school.math;
 
 import java.util.ArrayList;
 
-abstract public class AbstractNumberSequence extends ArrayList {
+abstract public class AbstractNumberSequence extends ArrayList<Double> {
 	public void calculateNextTo(int to, boolean next) {
 		this.ensureCapacity(to);
 		int i=this.size();
 		while (this.size()<to) {
-			if (next) {this.add(calculateNext(this.get(i)))}
+			if (next) {this.add(calculateNext(this.get(i)));}
 			else {this.add(calculateAt(i+1));}
 			i++;
 		}
 	}
 	//should be overridden
-	private double calculateNext(int prev) {
+	private double calculateNext(double prev) {
 		return prev+1;
 	}
 	private double calculateAt(int pos){
