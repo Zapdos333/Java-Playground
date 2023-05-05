dca6eb5fb24be214ad78456a26746b398b27787d
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-05-05 11:48:29 +0200

    added rhino-script-engine
    with integration to run.bat
    fixing evalConsole

--- com/Ace009/library/evalConsole.java
+++ com/Ace009/library/evalConsole.java
@@ -1,45 +1,52 @@
-//requires Overhaul and external ScriptEngines, currently only checks for all available ScriptEngines
-
 package com.Ace009.library;
 
-import java.util.Scanner;
+//import java.util.ArrayList;
 import java.util.Arrays;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngineFactory;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import javax.script.ScriptEngine;
+import org.mozilla.javascript.engine.RhinoScriptEngine;
+import org.mozilla.javascript.engine.RhinoScriptEngineFactory;
 
 public class evalConsole {
+	//base on: from "https://www.oracle.com/technical-resources/articles/javase/scripting.html"
+	public static void check() {
+		RhinoScriptEngineFactory factory=new RhinoScriptEngineFactory();
+		//for (RhinoScriptEngineFactory factory: factories) {
+			System.out.println("ScriptEngineFactory Info");
+			String engName = factory.getEngineName();
+			String engVersion = factory.getEngineVersion();
+			String langName = factory.getLanguageName();
+			String langVersion = factory.getLanguageVersion();
+			System.out.printf("\tScript Engine: %s (%s)\n", 
+					engName, engVersion);
+			List<String> engNames = factory.getNames();
+			for(String name: engNames) {
+				System.out.printf("\tEngine Alias: %s\n", name);
+			}
+			System.out.printf("\tLanguage: %s (%s)\n", 
+					langName, langVersion);
+		//}
+	}
 	public static void main(String[] args) {
-		if (args.length>0) {
-			System.out.println("Arguments: "+Arrays.toString(args));
+		if (args.length>0 && (Arrays.toString(args).contains("info"))) {
+			check();
+			System.out.println("");
 		}
 		Scanner scanner = new Scanner(System.in);
 		String input = new String();
-		ScriptEngineManager manager = new ScriptEngineManager();
-		ScriptEngine engine = manager.getEngineByName("javascript");
-		/*
+		RhinoScriptEngineFactory factory = new RhinoScriptEngineFactory();
+		ScriptEngine engine = factory.getScriptEngine();
 		while (true) {
 			System.out.print("Console input: ");
 			input=scanner.nextLine();
 			try {
 				Object result = engine.eval(input);
-				System.out.println("Returns: "+result.toString());
+				System.out.println("Returns: "+result.toString()+"\n");
 			} catch (Exception e) {
 				System.out.println("");
 				e.printStackTrace();
 			}
-			System.out.println("");
-			scanner.close();
-		} */
-		List<ScriptEngineFactory> factoriesList = manager.getEngineFactories();
-		ArrayList<List<String>> factorieNames = new ArrayList<List<String>>();
-		for (int i=0; i<factoriesList.size(); i++) {
-			factorieNames.set(i,factoriesList.get(i).getNames());
 		}
-		System.out.println(factorieNames.toString());
-		System.out.println(engine != null);
 	}
 }
