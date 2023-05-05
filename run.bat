94abb2e898e2a55fe96b2ffc408adcd8b4379019
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-05-05 11:48:29 +0200

    added rhino-script-engine
    with integration to run.bat
    fixing evalConsole

--- run.bat
+++ run.bat
@@ -1,9 +1,10 @@
 @echo off
+set jname=\recompile
 :start
 set /P jpath=please enter the path from \com\Ace009\ in "" : 
 echo compiling...
 :restart
-javac com\Ace009\%jpath%\*.java
+javac -cp ".;com\ext\*" com\Ace009\%jpath%\*.java
 echo files in path:
 echo:
 dir "com\Ace009\%jpath%\"
@@ -13,7 +14,7 @@ if %jname% equ \recompile (goto rerun)
 set /P jargs=arguments for main : 
 echo executing...
 echo:
-java "com.Ace009.%jpath:\=.%.%jname%" %jargs%
+java -cp ".;com\ext\*" "com.Ace009.%jpath:\=.%.%jname%" %jargs%
 echo:
 choice /c CRS /m "C for closing the window; R running again from same path; S for running a different file"
 if errorlevel 3 (goto start)
