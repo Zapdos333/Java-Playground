150d03fccceaa908ebb03c0d5e93e29f3803a5ea
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-05-10 08:49:12 +0200

    Update CString.java
    hotfix for correct length

--- com/Ace009/library/CClass/CString.java
+++ com/Ace009/library/CClass/CString.java
@@ -4,7 +4,7 @@ public class CString {
 	public static String formatToLength(String input, int length) {
 		StringBuilder output=new StringBuilder(input);
 		output.ensureCapacity(length);
-		while (output.length()<=length) {
+		while (output.length()<length) {
 			output.insert(0," ");
 		}
 		return output.toString();
