de5df546898568f0dc6716112186eadd03117443
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-05-15 21:21:01 +0200

    documentation for RNG

--- com/Ace009/library/RNG.java
+++ com/Ace009/library/RNG.java
@@ -2,51 +2,23 @@ package com.Ace009.library;
 
 import java.util.Random;
 
+/**
+ * a random number generator,
+ * that <code>extends Random</code>,
+ * has some additional instance methods
+ * @see Random
+ * @author Ace009
+ */
 public class RNG extends Random{
-	public int limitedIntRandom(int min, int max) {
-		return this.nextInt(max - min + 1) + min;
-	}
-	public double limitedDoubleRandom(double min, double max) {
-		return this.nextDouble()*((max)-min)+min;
-	}
-	public float limitedFloatRandom(float min, float max) {
-		return this.nextFloat()*((max)-min)+min;
-	}
-	public long limitedLongRandom(long min, long max) {
-		//yes were cheesing this with a double 0-1, because Random.nextLong() is not limitable and doesn't give all the same chance
-		double preRN=this.nextDouble();
-		long mult = ((max+1)-min)+min;
-		long rn = (long)(preRN*mult);
-		return rn;
-	}
-	public long rerollRandom(int length) { //Long max=9.223.372.036.854.775.808
-		StringBuilder seedS=new StringBuilder();
-		Long seed;
-		if (length>18) {length=18;}
-		for (int i = 0; i<length; i++) {
-			seedS.append(limitedIntRandom(0,9));
-		}
-		if (this.nextBoolean()) {
-			seedS.insert(0,"+");
-		} else {
-			seedS.insert(0,"-");
-		}
-		seed=Long.parseLong(seedS.toString());
-		this.setSeed(seed);
-		return seed;
-	}
-	public static void helpMain() {
-		//documentation output for main
-		System.out.println("Testing Function for Random Number generator");
-		System.out.println("first argument is for the amount of iterations");
-		System.out.println("second argument is the highest maximum number");
-		System.out.println("third argument is the seed length");
-		System.out.println("default values are: limit=10, max=10, seed length=18");
-	}
-	//debug Main function
+	/**
+	 * uses <code>args</code> as values to run specified iterations of random number generation.
+	 * @param args String Array:
+	 * @param args [0] iteration count (default:10)
+	 * @param args [1] highest number (default:10)
+	 * @param args [2] custom start seed length (default:15)
+	 */
 	public static void main(String[] args) {
 		RNG testRng= new RNG();
-		helpMain();
 		int limit;
 		int max;
 		int seedLength;
@@ -74,5 +46,83 @@ public class RNG extends Random{
 			System.out.println("Iteration: "+i);
 			System.out.println("Result: "+testRng.limitedFloatRandom(0,max));
 		}
+	}
+	/**
+	 * returns a random integer, based on <code>Random</code>
+	 * @param min
+	 * @param max
+	 * @return random integer
+	 * @see Random#nextInt(int)
+	 */
+	public int limitedIntRandom(int min, int max) {
+		return this.nextInt(max - min + 1) + min;
+	}
+	/**
+	 * returns a random double, based on <code>Random</code>
+	 * @param min
+	 * @param max
+	 * @return random double
+	 * @see Random#nextDouble()
+	 */
+	public double limitedDoubleRandom(double min, double max) {
+		return this.nextDouble()*((max)-min)+min;
+	}
+	/**
+	 * returns a random float, based on <code>Random</code>
+	 * @param min
+	 * @param max
+	 * @return random float
+	 * @see Random#nextFloat()
+	 */
+	public float limitedFloatRandom(float min, float max) {
+		return this.nextFloat()*((max)-min)+min;
+	}
+	/**
+	 * creates a random long by calling <code>nextDouble()</code>,
+	 * and the doing the old {@code (random)*(max-min)+min},
+	 * unreliable due to this attempting
+	 * @param min
+	 * @param max
+	 * @return 'random' long
+	 */
+	public long limitedLongRandom(long min, long max) {
+		//yes were cheesing this with a double 0-1, because Random.nextLong() is not limitable
+		double preRN=this.nextDouble();
+		long mult = (max-min)+min;
+		long rn = (long)(preRN*mult);
+		return rn;
+	}
+	/**
+	 * creates a random long by appending random integers to a {@code StringBuilder},
+	 * setting the sign with a {@code Random.nextBoolean()},
+	 * and then parsing it as {@code long}
+	 * @param length integer length
+	 * @return 'random' {@code long}
+	 */
+	public long rollLong (int length) { //Long max=9.223.372.036.854.775.808
+		StringBuilder outputS=new StringBuilder();
+		Long output;
+		if (length>18) {length=18;}
+		for (int i = 0; i<length; i++) {
+			outputS.append(limitedIntRandom(0,9));
+		}
+		if (this.nextBoolean()) {
+			outputS.insert(0,"+");
+		} else {
+			outputS.insert(0,"-");
+		}
+		output=Long.parseLong(outputS.toString());
+		return output;
+	}
+	/**
+	 * sets {@code Randoms} seed to the result of {@code rollLong(length)}
+	 * @param length integer length
+	 * @return the new seed
+	 * @see #rollLong(int)
+	 */
+	public long rerollRandom(int length) {
+		long seed=this.rollLong(length);
+		this.setSeed(seed);
+		return seed;
 	}
 }
