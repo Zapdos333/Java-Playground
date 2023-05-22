b6b1f2deb3fe805b5d3f03a551be97d93435e94f
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-05-22 21:39:27 +0200

    Change declarations of the members of TrackManager.java #7

--- com/Ace009/nonLibrary/Apk_Audioplayer/TrackManager.java
+++ com/Ace009/nonLibrary/Apk_Audioplayer/TrackManager.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import com.Ace009.library.CClass.CMath;
 import com.Ace009.library.CClass.CList;
 
-public final class TrackManager {
+public class TrackManager {
 	//define Music Track Object
-	public final class Track {
+	public static class Track {
 		String fName;	//file name
 		String fPath; //file path
 		int[] length; //array [sec,min]
@@ -30,11 +30,11 @@ public final class TrackManager {
 			System.out.println("Result: "+CMath.limitedRandom(10,20));
 		}
 	}
-	ArrayList<Integer> history=new ArrayList<>();	//list of ids in allTracks
-	ArrayList<Integer> playlist=new ArrayList<>();	//""
-	ArrayList<Track> allTracks=new ArrayList<>();
+	public static ArrayList<Integer> history=new ArrayList<>();	//list of ids in allTracks
+	public static ArrayList<Integer> playlist=new ArrayList<>();	//""
+	public static ArrayList<Track> allTracks=new ArrayList<>();
 	//reset playlist method
-	public void reset(boolean full) {
+	public static void reset(boolean full) {
 		if (full) {
 			allTracks.forEach(element->{
 				element.fitsToId=new ArrayList<>();
@@ -46,7 +46,7 @@ public final class TrackManager {
 		history=new ArrayList< >();
 	}
 	//method to get a random next Track based on fits
-	public Track getNextTrack(Track currTrack) {
+	public static Track getNextTrack(Track currTrack) {
 		int output;
 		if (CMath.limitedRandom(0,3)>1) {
 			output=CList.getRandom(currTrack.fitsToId);
@@ -56,7 +56,7 @@ public final class TrackManager {
 		return allTracks.get(output);
 	}
 	//method to set the values int 
-	public void setFitValue(int fitValue, int currTrack, int nextTrack) {
+	public static void setFitValue(int fitValue, int currTrack, int nextTrack) {
 		switch(fitValue) {
 			case 1:
 				allTracks.get(currTrack).fitsToId.add(playlist.get(nextTrack));
