f127915466538dacacc935679f0a33f7634ef6b1
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-05-26 11:40:57 +0200

    Created purge_cache.py
    delets directory, where name fits scan.test
    update pyScan to support directory tasks

---
+++ purge_cache.py
@@ -0,0 +1,18 @@
+#imports
+import shutil,re
+from python_imports import pyScan
+scan=pyScan
+#defenitions
+def deletion(filePath:str):
+	shutil.rmtree(filePath)
+	print(filePath,"deleted")
+#default setup
+scan.test=re.compile(r"__pycache__|\.history")
+scan.exclude=[".venv",".git"]
+#when run from command line
+if __name__ == "__main__":
+	import sys
+	if len(sys.argv)>1:
+		pass
+print(scan.test)
+scan.recursiveScanRun(".",deletion,True)
--- python_imports/pyScan.py
+++ python_imports/pyScan.py
@@ -8,7 +8,7 @@ def deduplicate(List:list):
 			output.append(List[i])
 	return output
 #
-def scanPulseRun(f_paths:list[str], f_scanned:list[str], task):
+def scanPulseRun(f_paths:list[str], f_scanned:list[str], task, scandir:bool=False):
 	output=[]
 	for path in f_paths:
 		with os.scandir(path) as it:
@@ -17,23 +17,24 @@ def scanPulseRun(f_paths:list[str], f_scanned:list[str], task):
 				if entry.is_dir():
 					if entry.name not in f_scanned:
 						output.append(os.path.join(path,entry.name))
-				elif entry.is_file() and re.search(test,entry.name):
+						if scandir and re.search(test,entry.name): task(os.path.join(path,entry.name))
+				if entry.is_file() and re.search(test,entry.name) and not scandir:
 					task(os.path.join(path,entry.name))
 					print("executed on:",os.path.join(path,entry.name),end="\n\n")
 		f_scanned.append(path)
 	output=deduplicate(output)
 	return output
 #
-def recursiveScanRun(origin:str,task):
+def recursiveScanRun(origin:str,task,scandir:bool=False):
 	results=[]
 	scanned=exclude
-	results=scanPulseRun([origin],scanned,task)
+	results=scanPulseRun([origin],scanned,task,scandir)
 	while len(results)>0:
 		new_results=[]
 		for result in results:
 			if os.path.isdir(result):
 				new_results.append(result)
-		results=scanPulseRun(new_results,scanned,task)
+		results=scanPulseRun(new_results,scanned,task,scandir)
 		scanned.extend(results)
 		print("found in cycle:\n",results,sep="",end="\n\n")
 		scanned=deduplicate(scanned)
