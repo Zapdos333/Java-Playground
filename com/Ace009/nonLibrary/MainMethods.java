6fc47917fbba1c2cdd1112e6da3beefa764196e7
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-01 12:29:31 +0200

    implement Args in all Main

--- com/Ace009/nonLibrary/MainMethods.java
+++ com/Ace009/nonLibrary/MainMethods.java
@@ -17,12 +17,10 @@ public class MainMethods {
 	 * prints the iterator created by 
 	 * {@link com.Ace009.library.Range#arrayRange(int,int,int)}
 	 * in new lines
-	 * @param args is irrelevant,
-	 * uses {@link com.Ace009.library.Args}
 	 * 
 	 * @see com.Ace009.library.Range
 	 */
-	public static void rangeMain(String[] args) {
+	public static void rangeMain() {
 		Args arguments = new Args("int", "Stop","Start","Steps");
 		int stop = arguments.outputInt[0];
 		int start = arguments.outputInt[1];
@@ -34,66 +32,65 @@ public class MainMethods {
 	/**
 	 * {@code RNG}s main method:
 	 * <p>
-	 * uses <code>args</code> as values to run specified iterations of random number generation.
-	 * @param args String Array:
-	 * @param args [0] iteration count (default:10)
-	 * @param args [1] highest number (default:10)
-	 * @param args [2] custom start seed length (default:15)
+	 * runs specified iterations of random number generation.
 	 * 
 	 * @see com.Ace009.library.RNG
 	 */
-	public static void RNGMain(String[] args) {
+	public static void RNGMain() {
+		//create new RNG
 		RNG testRng= new RNG();
-		int limit;
-		int max;
-		int seedLength;
-		//checking for custom values, else use defaults
-		if (args.length < 1) {
-			limit=10;
-		} else {
-			limit=Integer.parseInt(args[0]);
-			System.out.println("Set limit to custom: "+limit);
-		} if (args.length < 2) {
-			max=10;
-		} else {
-			max=Integer.parseInt(args[1]);
-			System.out.println("Set max to custom: "+max);
-		} if (args.length < 3) {
-			seedLength=15;
-		} else {
-			seedLength=Integer.parseInt(args[2]);
-			System.out.println("Set seedLength to custom: "+seedLength);
-		}
+		//ask for Args
+		Args input = new Args("int","amount","max","Seed length");
+		//insert defaults
+		input.parseWithDefaults(new int[]{10,10,15});
+		//set parameters
+		int limit = input.outputInt[0];
+		int max = input.outputInt[1];
+		int seedLength = input.outputInt[2];
+		//ask for type of number
+		String type = new Args("String", "type of numbers").output[0].toLowerCase();
 		//reroll for seed
 		System.out.println("new Seed is: "+testRng.rerollRandom(seedLength));
 		//output requested random numbers
 		for (int i = 1; i <= limit; i++) {
-			System.out.println("Iteration: "+i);
-			System.out.println("Result: "+testRng.limitedFloatRandom(0,max));
+			switch (type) {
+				case "long":
+					System.out.printf("Iteration: %d; RNG: %d",i,testRng.limitedLongRandom(0,max));
+					break;
+				case "double":
+					System.out.printf("Iteration: %d; RNG: %f",i,testRng.limitedDoubleRandom(0,max));
+					break;
+				case "float":
+					System.out.printf("Iteration: %d; RNG: %f",i,testRng.limitedFloatRandom(0,max));
+					break;
+				case "int":
+				case "integer":
+				default:
+					System.out.printf("Iteration: %d; RNG: %d",i,testRng.limitedIntRandom(0,max));
+					break;
+			}
 		}
 	}
 	/**
-	 * {@code RNG}s main method:
+	 * {@code Circle}s main method:
 	 * <p>
-	 * 
-	 * @param args String Array:
-	 * @param args [0] Corners
+	 * creates a test circle with the given parameters,
+	 * prints out the list of coordinates, and the 'Circularity'
 	 */
-	public static void CircleMain(String[] args) {
+	public static void CircleMain() {
+		Args input = new Args("double", "X","Y","Radius","Corners");
+		input.parseWithDefaults(new double[] {5,5,5,10});
 		System.out.println("Test program: ");
-		System.out.println("1: Corners");
-		int aC= Integer.parseInt(args[0]);
-		double aX= 5;
-		double aY= 5;
-		double aR= 5;
+		double[] Nargs = input.outputDouble;
+		int aC = (int)Math.floor(Nargs[3]);
+		double aX= Nargs[0];
+		double aY= Nargs[1];
+		double aR= Nargs[2];
 		Circle test = new Circle(new DoubleCoordinate(aX,aY),aR);
-		System.out.print("CenterX="+test.center.x);
-		System.out.print(" CenterY="+test.center.y);
-		System.out.println(" Radius="+test.radius);
 		ArrayList<DoubleCoordinate> result = test.constructPoly(aC);
 		System.out.println("Circle: "+result.toString());
-		System.out.println("Circluarity: "+test.circumferance()+"/"+DoubleCoordinate.totalDistance(result,true)+"="+Circle.getCircularity(result,aR));
-		System.out.println("Corners: "+result.size());
+		System.out.println("Circluarity: "+test.circumferance()+"/"
+		+DoubleCoordinate.totalDistance(result,true)+"="+Circle.getCircularity(result,aR));
 	}
 	public static void main(String[] args) {
 		if (args.length==0) {
@@ -115,15 +112,15 @@ public class MainMethods {
 		switch (type) {
 			case "circle":
 			case "circlemain":
-				CircleMain(Nargs);
+				CircleMain();
 				break;
 			case "rng":
 			case "rngmain":
-				RNGMain(Nargs);
+				RNGMain();
 				break;
 			case "range":
 			case "rangemain":
-				rangeMain(Nargs);
+				rangeMain();
 				break;
 			default: System.out.println("No implemented type given. type: "+type);
 		}
