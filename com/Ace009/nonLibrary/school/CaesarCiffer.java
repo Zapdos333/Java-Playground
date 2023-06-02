4808585cdcf53fa3517a4c799f4d685820fc4830
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-02 21:51:41 +0200

    Update build.gradle
    finally working, with two modules
    also, hotfix CaesarCiffer

--- com/Ace009/nonLibrary/school/CaesarCiffer.java
+++ com/Ace009/nonLibrary/school/CaesarCiffer.java
@@ -6,7 +6,7 @@ import com.Ace009.library.Args;
 public class CaesarCiffer {
 	static char[] alphabet = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 	private char[] ciffer = new char[alphabet.length];
-	public CaesarCiffre(int code) {
+	public CaesarCiffer(int code) {
 
 	}
 }
