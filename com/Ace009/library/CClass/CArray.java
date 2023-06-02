92eeeb3388ea92d2fb6813be1d0bbaa0e432a9de
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-02 19:52:30 +0200

    temp
    start CaesarCiffer

---
+++ com/Ace009/library/CClass/CArray.java
@@ -0,0 +1,15 @@
+package com.Ace009.library.CClass;
+
+/**
+ * final 'static' class,
+ * one of the 'C(ustom)Class' libaries,
+ * this one works with Arrays {@code T[]}
+ * @author Ace009
+ */
+public class CArray {
+	/**
+	 * don't
+	 * @see CList
+	 */
+	private CArray() {}
+}
---
+++ com/Ace009/nonLibrary/school/CaesarCiffer.java
@@ -0,0 +1,12 @@
+package com.Ace009.nonLibrary.school;
+
+import com.Ace009.library.Range;
+import com.Ace009.library.Args;
+
+public class CaesarCiffer {
+	static char[] alphabet = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	private char[] ciffer = new char[alphabet.length];
+	public CaesarCiffre(int code) {
+
+	}
+}
