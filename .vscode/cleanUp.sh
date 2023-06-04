bf7f6300cea50ca20baa23efed1a523ad8a4fa2f
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-04 18:40:52 +0200

    move python to src

--- .vscode/cleanUp.sh
+++ .vscode/cleanUp.sh
@@ -1,3 +1,3 @@
-py python_files/purge_class.py
-py python_files/purge_class.py .html
-py python_files/purge_cache.py
+py src/python/purge_class.py
+py src/python/purge_class.py .html
+py src/python/purge_cache.py
