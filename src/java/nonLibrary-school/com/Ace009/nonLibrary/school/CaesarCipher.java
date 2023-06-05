ce85d6976bb41436029ef23e9cc4b23d7291eb95
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-05 17:58:22 +0200

    temp CaesarCipher and CArray

--- src/java/nonLibrary-school/com/Ace009/nonLibrary/school/CaesarCipher.java
+++ src/java/nonLibrary-school/com/Ace009/nonLibrary/school/CaesarCipher.java
@@ -2,11 +2,34 @@ package com.Ace009.nonLibrary.school;
 
 //import com.Ace009.library.Range;
 //import com.Ace009.library.Args;
+import com.Ace009.library.CClass.CArray;
 
 public class CaesarCipher {
-	static char[] alphabet = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	private char[] cipher = new char[alphabet.length];
-	public CaesarCipher(int code) {
-
+	/**a {@code char[]} containing the latin alphabet */
+	static final char[] ALPHABET = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	/**securing the alphabet */
+	static {
+		if(ALPHABET.length!=26) {throw new SecurityException("altered constant field");}
+	}
+	/**this instances warped {@link #alphabet} constant */
+	private final char[] cipher = new char[ALPHABET.length];
+	/**
+	 * 
+	 * @param code
+	 */
+	public CaesarCipher(final byte code) {
+		byte i=0;
+		while (cipher[25]=='\u0000') {
+			cipher[i+code]=ALPHABET[i];
+			i++;
+		}
+		i=0;
+		while (i<code) {
+			cipher[i]=ALPHABET[i];
+			i++;
+		}
+	}
+	public CaesarCipher (char code) {
+		this((byte)CArray.indexOf(CArray.asObjectArray(ALPHABET), code));
 	}
 }
