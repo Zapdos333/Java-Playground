d46d91094d7e127c556dacc8ff6c75cac9a3b776
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-13 08:17:10 +0200

    set up command line wrapper

--- src/java/nonLibrary-tests/com/Ace009/nonLibrary/debug/CommandLineWrapper.java
+++ src/java/nonLibrary-tests/com/Ace009/nonLibrary/debug/CommandLineWrapper.java
@@ -1,7 +1,21 @@
 package com.Ace009.nonLibrary.debug;
 
+import com.Ace009.library.Args;
+
 public class CommandLineWrapper {
 	public static void main(String[] args) {
-
+		boolean running = true;
+		String command;
+		while (running) {
+			command = Args.ask("Command").toLowerCase();
+			switch (command) {
+				case"stop": running=false; break;
+				case"run":
+				case"main":
+					System.out.println("running MainMethods...");
+					MainMethods.main(args); break;
+			}
+		}
+		System.out.println("stopping...");
 	}
 }
