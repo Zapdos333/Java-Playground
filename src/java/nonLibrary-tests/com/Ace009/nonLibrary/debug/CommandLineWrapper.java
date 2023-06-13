7dcd929f6e43427d54a10fd44ffd8d04bea6af39
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-13 09:04:20 +0200

    javadocs in commandlinewrapper

--- src/java/nonLibrary-tests/com/Ace009/nonLibrary/debug/CommandLineWrapper.java
+++ src/java/nonLibrary-tests/com/Ace009/nonLibrary/debug/CommandLineWrapper.java
@@ -2,19 +2,34 @@ package com.Ace009.nonLibrary.debug;
 
 import com.Ace009.library.Args;
 
+/**
+ * Command line wrapper for {@link MainMethods}
+ * @author Ace009
+ */
+ // and potentially other debug methods
 public class CommandLineWrapper {
+	/** don't */
+	private CommandLineWrapper() {}
+	/**
+	 * system default main method, uses {@link Args#ask(String)} to run specifed commands
+	 * @param args default system arguments, will de used when calling a main method
+	 */
 	public static void main(String[] args) {
 		boolean running = true;
 		String command;
+		System.out.println("\nRunning MainMethod CommandLineWrapper:");
 		while (running) {
-			command = Args.ask("Command").toLowerCase();
+			command = Args.ask("\nCommand").toLowerCase();
 			switch (command) {
-				case"stop": running=false; break;
+				case"stop":
+				case"break":
+					running=false; break;
 				case"run":
 				case"main":
 					System.out.println("running MainMethods...");
 					MainMethods.main(args); break;
 			}
+			System.out.println("\nreturning to Wrapper...");
 		}
 		System.out.println("stopping...");
 	}
