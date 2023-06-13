4f4d98cd5d30c0ffc1cb901d644a974d91b56a0f
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-13 13:49:37 +0200

    Implement cobject in CommandLineWrapper

--- src/java/nonLibrary-tests/com/Ace009/nonLibrary/debug/CommandLineWrapper.java
+++ src/java/nonLibrary-tests/com/Ace009/nonLibrary/debug/CommandLineWrapper.java
@@ -1,6 +1,11 @@
 package com.Ace009.nonLibrary.debug;
 
-import com.Ace009.library.Args;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.Ace009.library.*;
+import com.Ace009.library.CClass.*;
+//import com.Ace009.library.CoordinateSystem.*;
 
 /**
  * Command line wrapper for {@link MainMethods}
@@ -23,14 +28,48 @@ public class CommandLineWrapper {
 			switch (command) {
 				case"stop":
 				case"break":
-					running=false; break;
+					running=false;
+					System.out.println("stopping...");
+					return;
 				case"run":
 				case"main":
 					System.out.println("running MainMethods...");
 					MainMethods.main(args); break;
+				case"object":
+				case"cobject":
+					System.out.println("running CObject Test...");
+					CobjectTest(); break;
 			}
 			System.out.println("\nreturning to Wrapper...");
 		}
-		System.out.println("stopping...");
+	}
+	/**
+	 * 
+	 */
+	public static void CobjectTest() {
+		String type = Args.ask("type").toLowerCase();
+		Object test;
+		System.out.println("Getting test object...");
+		switch (type) {
+			case "args":
+				int number = Integer.parseInt(Args.ask("Number of Args(numerical)"));
+				String[] askKey = new String[number];
+				for (int i = 0; i < number; i++) {
+					askKey[i] = i+". Entry";
+				}
+				test = new Args(Args.OutputType.String, askKey);
+				break;
+			case "rng":
+			case "random":
+				test= new RNG();
+				System.out.printf("Seed: %d\n", ((RNG) test).rerollRandom(18));
+				break;
+			default:
+				System.out.println("No implemented Object Type given,"); return;
+		}
+		Map<String, Object> output = new HashMap<>();
+		try{ output = CObject.entriesMap(test);}
+		catch (IllegalAccessException e) { e.printStackTrace(); }
+		CMap.print(output);
 	}
 }
