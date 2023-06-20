d8796e45cca2298fef4d816a04c0194c648a6ff7
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-20 21:57:09 +0200

    add Stream-magic to CList

--- src/java/library-CClass/com/Ace009/library/CClass/CList.java
+++ src/java/library-CClass/com/Ace009/library/CClass/CList.java
@@ -1,17 +1,16 @@
 package com.Ace009.library.CClass;
 
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Collection;
+//import java.util.ArrayList;
+import java.util.stream.Stream;
+
 /**
  * 'static' class,
  * one of the 'C(ustom)Class' libaries,
  * this one works with {@link ArrayList}
  * <p> tries it's best to work Interfaces as I/O
- * to work for more different {@link Collection}/{@link List}
+ * to work for different {@link List}
  * @author Ace009
- * @see java.util.ArrayList
- * @see java.util.Collection
  * @see java.util.List
  */
 public class CList {
@@ -23,28 +22,22 @@ public class CList {
 	/**
 	 * returns a random entry from the list
 	 * @param <T> the type of the lists entries
-	 * @param list {@link ArrayList} of type {@code T}
+	 * @param list {@link List} of type {@code T}
 	 * @return {@code Type}: a random entry from {@code list}
 	 */
 	public static <T> T getRandom(List<T> list) {
 		return list.get((int)(Math.random() * list.size()));
 	}
 	/**
-	 * deduplicates the {@code list} by adding the entries
-	 * to a new {@code List} one by one and checking 
-	 * that the new entry isn't already in the new {@code List},
-	 * relies on {@code Object.equals}
+	 * deduplicates the {@code list} by filtering out all entries that
+	 * are more than once in the {@code List},
+	 * <p> uses {@link Stream} magic
 	 * @param <T> the type of the lists entries
 	 * @param list the list to deduplicate
 	 * @return a new {@code List} with only one of each entry
 	 */
-	public static <T> List<T> deduplicate(Collection<T> list) {
-		List<T> output = new ArrayList<>(list.size());
-		for (T entry : list) {
-			if (!output.contains(entry)) {
-				output.add(entry);
-			}
-		}
-		return output;
+	public static <T> List<T> deduplicate(List<T> list) {
+		list.removeIf(e->list.stream().filter(x->x==e).toArray().length<2);
+		return list;
 	}
 }
