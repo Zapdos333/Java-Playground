d0e8831d2330edddedeba0dbf0bdbd2ab9c96bf6
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-23 12:47:14 +0200

    removed CList.deduplicate
    redundent due to easy source code replicatibility

--- src/java/library-CClass/com/Ace009/library/CClass/CList.java
+++ src/java/library-CClass/com/Ace009/library/CClass/CList.java
@@ -1,7 +1,6 @@
 package com.Ace009.library.CClass;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * 'static' class,
@@ -24,16 +23,5 @@ public class CList {
 	 */
 	public static <T> T getRandom(List<T> list) {
 		return list.get((int)(Math.random() * list.size()));
-	}
-	/**
-	 * deduplicates the {@code list} by filtering out all entries that
-	 * are more than once in the {@code List},
-	 * <p> uses {@link Stream#distinct()}
-	 * @param <T> the type of the lists entries
-	 * @param list the list to deduplicate
-	 * @return a new {@code List} with only one of each entry
-	 */
-	public static <T> List<T> deduplicate(List<T> list) {
-		return list.stream().distinct().toList();
 	}
 }
