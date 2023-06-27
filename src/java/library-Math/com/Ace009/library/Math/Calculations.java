5244e06905029ca2ce6fcfb114844711a494a75e
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-27 22:28:46 +0200

    fix Calculations.gcd (uses now frequency Maps)
    implement CArray.frequencyMap

--- src/java/library-Math/com/Ace009/library/Math/Calculations.java
+++ src/java/library-Math/com/Ace009/library/Math/Calculations.java
@@ -2,7 +2,7 @@ package com.Ace009.library.Math;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.stream.IntStream;
+//import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.Map;
 import java.util.HashMap;
@@ -45,9 +45,23 @@ public class Calculations {
 	 */
 	public static int gcd(int n1, int n2) {
 		int[] p1 = seperateToPrimes(n1);
-		Integer[] p2 = CArray.asObjectArray(seperateToPrimes(n2));
-		return IntStream.of(p1)
-			.filter(i->CArray.indexOf(p2, i)>-1)
+		int[] p2 = seperateToPrimes(n2);
+		Map<Integer, Integer> fMap1 = CArray.frequencyMap(CArray.asObjectArray(p1));
+		Map<Integer, Integer> fMap2 = CArray.frequencyMap(CArray.asObjectArray(p2));
+		Map<Integer, Integer> frequencyMap = new HashMap<>(
+			Stream.concat(
+				fMap1.entrySet().stream().map(Map.Entry::getKey),
+				fMap2.entrySet().stream().map(Map.Entry::getKey)
+			).distinct().toArray().length
+		);
+		for (Map.Entry<Integer,Integer> e : fMap1.entrySet()) {
+			frequencyMap.put(e.getKey(),Math.min(e.getValue(),fMap2.getOrDefault(e.getKey(),0)));
+		}
+		for (Map.Entry<Integer,Integer> e : fMap2.entrySet()) {
+			frequencyMap.put(e.getKey(),Math.min(e.getValue(),fMap1.getOrDefault(e.getKey(),0)));
+		}
+		return frequencyMap.entrySet().stream()
+			.mapToInt(e->(int)Math.pow(e.getKey(),e.getValue()))
 			.reduce(1,(a,b)->a*b);
 	}
 	/**
@@ -59,14 +73,8 @@ public class Calculations {
 	public static int lcm(int n1, int n2) {
 		int[] p1 = seperateToPrimes(n1);
 		int[] p2 = seperateToPrimes(n2);
-		Map<Integer, Integer> fMap1 = new HashMap<>(IntStream.of(p1).distinct().toArray().length);
-		Map<Integer, Integer> fMap2 = new HashMap<>(IntStream.of(p2).distinct().toArray().length);
-		for (int e : p1) {
-			fMap1.put(e,fMap1.getOrDefault(e,0)+ 1);
-		}
-		for (int e : p2) {
-			fMap2.put(e,fMap2.getOrDefault(e,0)+ 1);
-		}
+		Map<Integer, Integer> fMap1 = CArray.frequencyMap(CArray.asObjectArray(p1));
+		Map<Integer, Integer> fMap2 = CArray.frequencyMap(CArray.asObjectArray(p2));
 		Map<Integer, Integer> frequencyMap = new HashMap<>(
 			Stream.concat(
 				fMap1.entrySet().stream().map(Map.Entry::getKey),
