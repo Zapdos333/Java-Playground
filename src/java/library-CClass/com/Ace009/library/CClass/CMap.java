4350db34f3b7d7f9cf56d08c1ed945432a1363a7
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-06-29 19:18:25 +0200

    Update CMap for more variable types

--- src/java/library-CClass/com/Ace009/library/CClass/CMap.java
+++ src/java/library-CClass/com/Ace009/library/CClass/CMap.java
@@ -22,10 +22,10 @@ public class CMap {
 	 * @return a {@code String[]} containing the printable output seperated in lines
 	 * @see CString#formatToLength(String, int)
 	 */
-	public static String[] print(Map<String, Object> map, String keys, String values) {
+	public static String[] print(Map<String, ?> map, String keys, String values) {
 		String[] output = new String[map.size()+1];
 		int keyLength=0; int valueLength=0;
-		for (Map.Entry<String,Object> e : map.entrySet()) {
+		for (Map.Entry<String,?> e : map.entrySet()) {
 			keyLength = Math.max(keyLength,e.getKey().length());
 			valueLength = Math.max(valueLength,e.getValue().toString().length());
 		}
@@ -33,7 +33,7 @@ public class CMap {
 			keys,CString.formatToLength("", keyLength-keys.length()),
 			values,CString.formatToLength("", valueLength-values.length()));
 		int i = 1;
-		for (Map.Entry<String, Object> entry : map.entrySet()) {
+		for (Map.Entry<String, ?> entry : map.entrySet()) {
 			output[i] = String.format("|%s:|%s|",
 				CString.formatToLength(entry.getKey(),keyLength),
 				CString.formatToLength(entry.getValue().toString(),valueLength)
@@ -48,15 +48,17 @@ public class CMap {
 	 * @return a {@code String[]} containing the printable output seperated in lines
 	 * @param map the map to print
 	 */
-	public static String[] print(Map<String, Object> map){ return print(map,"Keys","Values"); }
+	public static String[] print(Map<String, ?> map){ return print(map,"Keys","Values"); }
 	/**
 	 * Maps the input arrays as keys and values into the returned {@code Map}
-	 * @param keys {@code String[]} the keys array
-	 * @param values {@code Object[]} the values array
+	 * @param <K> {@code <K>} the type of the keys
+	 * @param <V> {@code <V>} the type of the values
+	 * @param keys {@code K[]} the keys array
+	 * @param values {@code V[]} the values array
 	 * @return the returned {@code Map}
 	 */
-	public static Map<String,Object> fromArray(String[] keys, Object[] values) {
-		Map<String, Object> output = new HashMap<>();
+	public static <K,V> Map<K,V> fromArray(K[] keys, V[] values) {
+		Map<K,V> output = new HashMap<>();
 		assert keys.length == values.length;
 		for (int i = 0; i < keys.length; i++) {
 			output.put(keys[i], values[i]);
@@ -64,15 +66,19 @@ public class CMap {
 		return output;
 	}
 	/**
-	 * maps the input array as keys and values into the returned {@code Map<String,Object>}
-	 * <p>(uses keys {@link Object#toString()} for the String keys)
-	 * @param entries an {@code Object[][]} array containing Touples of [key, value]
-	 * @return the mapped input
+	 * maps the input array as keys and values into the returned {@code Map<K, V>}
+	 * <p> uses the first entry of each subarray as key and the second entry as value
+	 * @param <T> {@code <T>} the type of the input array
+	 * @param <K> {@code <K>} the type of the keys, must extend {@code <T>}
+	 * @param <V> {@code <V>} the type of the values, must extend {@code <T>}
+	 * @param entries an {@code T[][]} array containing Touples of [key, value]
+	 * @return a Map of the input
 	 */
-	public static Map<String, Object> fromArray(Object[][] entries) {
-		Map<String, Object> output = new HashMap<>();
-		for (Object[] entry : entries) {
-			output.put(entry[0].toString(),entry[1]);
+	@SuppressWarnings("unchecked") // for K, V type casting
+	public static <K extends T, V extends T, T> Map<K, V> fromArray(T[][] entries) {
+		Map<K, V> output = new HashMap<>();
+		for (T[] entry : entries) {
+			output.put((K)entry[0],(V)entry[1]);
 		}
 		return output;
 	}
