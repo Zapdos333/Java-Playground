e4ec634ea50a151f818632bddcbdbe652f2886a3
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-07-07 17:18:30 +0200

    Update Log.java
    added error stream to stream (used for Level.nr>2)
    added timestamps and errorLevels to Log output

--- src/java/library-general/com/Ace009/library/Log.java
+++ src/java/library-general/com/Ace009/library/Log.java
@@ -1,6 +1,7 @@
 package com.Ace009.library;
 
 import java.io.PrintStream;
+import java.util.Calendar;
 
 /**
  * a class for selective log output
@@ -8,7 +9,9 @@ import java.io.PrintStream;
  */
 public class Log {
 	/** the default output PrintStream {@link System#out} */
-	public static final PrintStream output = System.out;
+	public static final PrintStream OUT = System.out;
+	/** the default output PrintStream {@link System#err} */
+	public static final PrintStream ERROR = System.err;
 	/** the Level of an output */
 	public static enum Level {
 		DEBUG((byte)0), INFO((byte)1), WARNING((byte)2), ERROR((byte)3), FATAL((byte)4);
@@ -22,19 +25,34 @@ public class Log {
 	/** @return the global {@link #outputLevel output configuration} */
 	public static Level getOutputLevel() { return outputLevel; }
 	/**
-	 * prints the message into {@link #output},
+	 * prints the message into {@link #OUT} or {@link #ERROR},
 	 * if {@code level} is larger or equal to {@link #outputLevel}
 	 * <p> uses {@link PrintStream#println(String)}
+	 * <p> uses the format {@code [hour:minute:second]:[level]:message}
+	 * @param level the output level of the message
+	 * @param message the message
 	 */
 	public static void out(Level level, String message) {
-		if (level.nr >= Log.outputLevel.nr) Log.output.println(message);
+		Calendar time = Calendar.getInstance();
+		String t_ = String.format("[%d:%d:%d]:[%s]:%s",
+			time.get(Calendar.HOUR_OF_DAY),time.get(Calendar.MINUTE),time.get(Calendar.SECOND),
+			level.name(),message
+		);
+		if (level.nr >= Log.outputLevel.nr) {
+			if (level.nr < 3) Log.OUT.println(t_);
+			else Log.ERROR.println(t_);
+		}
 	}
 	/**
-	 * prints the message into {@link #output},
+	 * prints the message into {@link #OUT} or {@link #ERROR},
 	 * if {@code level} is larger or equal to {@link #outputLevel},
-	 * <p>uses {@link PrintStream#printf(String, Object[])}
+	 * <p> relays the format and args into {@link String#format(String, Object[]) String.format(String, Object...)},
+	 * and then into {@link #out(String)}
+	 * @param level the output level of the message
+	 * @param format the message according to {@code String.format(String, Object[])}
+	 * @param args for the formatting
 	 */
 	public static void out(Level level, String format, Object...args) {
-		if (level.nr >= Log.outputLevel.nr) Log.output.printf(format,args);
+		Log.out(level,String.format(format, args));
 	}
 }
