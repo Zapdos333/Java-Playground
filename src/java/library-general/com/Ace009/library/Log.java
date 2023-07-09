7b060e92501bd1aa540dd4a5130dda1c91f8db1e
Zapdos333 <53367686+Zapdos333@users.noreply.github.com>
2023-07-09 17:09:44 +0200

    Update Log.java
    made time always 2-digit

--- src/java/library-general/com/Ace009/library/Log.java
+++ src/java/library-general/com/Ace009/library/Log.java
@@ -8,16 +8,16 @@ import java.util.Calendar;
  * @author Ace009
  */
 public class Log {
-	/** the default output PrintStream {@link System#out} */
-	public static final PrintStream OUT = System.out;
-	/** the default error PrintStream {@link System#err} */
-	public static final PrintStream ERROR = System.err;
 	/** the Level of an output */
 	public static enum Level {
 		DEBUG((byte)0), INFO((byte)1), USER_INPUT((byte)1), WARNING((byte)2), ERROR((byte)3), FATAL((byte)4);
 		byte nr;
 		private Level(byte nr) { this.nr=nr; }
 	}
+	/** the default output PrintStream {@link System#out} */
+	public static final PrintStream OUT = System.out;
+	/** the default error PrintStream {@link System#err} */
+	public static final PrintStream ERROR = System.err;
 	/** the current global(static) configuration of which output gets printed */
 	protected static Level outputLevel = Level.INFO;
 	/** sets the global {@link #outputLevel output configuration} */
@@ -26,17 +26,16 @@ public class Log {
 	public static Level getOutputLevel() { return outputLevel; }
 	/**
 	 * prints the message into {@link #OUT} or {@link #ERROR},
-	 * if {@code level} is larger or equal to {@link #outputLevel}
+	 * if {@code level} is larger or equal then {@link #outputLevel}
 	 * <p> uses {@link PrintStream#println(String)}
 	 * <p> uses the format {@code [hour:minute:second]:[level]:message}
 	 * @param level the output level of the message
 	 * @param message the message
 	 */
 	public static void out(Level level, String message) {
-		Calendar time = Calendar.getInstance();
-		String t_1 = String.format("[%d:%d:%d]:[%s]:%s",
-			time.get(Calendar.HOUR_OF_DAY),time.get(Calendar.MINUTE),time.get(Calendar.SECOND),
-			level.name(),message
+		String t_1 = String.format("[%s:%s:%s]:[%s]:%s",
+			getTime("hour"),getTime("minute"),getTime("second"),
+			level.name().replace('_',' '),message
 		);
 		String[] t_2 = t_1.split("\n");
 		if (level.nr >= Log.outputLevel.nr) {
@@ -58,5 +57,21 @@ public class Log {
 	 */
 	public static void out(Level level, String format, Object...args) {
 		Log.out(level,String.format(format, args));
+	}
+	/**
+	 * returns a string representation of the time field set to two digits
+	 * <p> if {@code field} is neither {@code "second", "minute", "hour"} "00" is returned
+	 * @param field the name of the time fragment
+	 * @return a 2-digit string representing the specified time fragment
+	 */
+	private static String getTime(String field) {
+		Calendar time = Calendar.getInstance();
+		String t_ = "00";
+		switch(field.toLowerCase()) {
+			case "second": t_=Integer.toString(time.get(Calendar.SECOND));
+			case "minute": t_=Integer.toString(time.get(Calendar.MINUTE));
+			case "hour": t_=Integer.toString(time.get(Calendar.HOUR_OF_DAY));
+		}
+		if (t_.length()<2) t_ = "0"+t_; return t_;
 	}
 }
